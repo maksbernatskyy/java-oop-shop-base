658fbe1bccaa964dfd49203482ed3167e2bbf350
maksbernatskyy <maksbernatskyy@gmail.com>
2026-02-13 14:00:57 +0100

    first commit

---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,12 @@
+package org.lessons.java.shop;
+
+public class Prodotto {
+    // Attributi
+    int codice;
+    String nome;
+    String descrizione;
+    Float prezzo;
+    String iva;
+
+    // Metodi
+}
